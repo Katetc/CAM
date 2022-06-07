08032b73db277afea298e661e84f467f530f57ce
huebler <huebler@uwm.edu>
2022-06-07 18:15:47 -0500

    Making silhs use the new 2D version of setup_grid. This also inadvertently fixes a bug added in commit c816bcbd4bb8b8cfa89aa9c0f976ee799cb795e2. The issue was that we used pver when we wanted pverp.

--- src/physics/cam/subcol_SILHS.F90
+++ src/physics/cam/subcol_SILHS.F90
@@ -675,7 +675,7 @@ contains
       real(r8), parameter :: qsmall = 1.0e-18_r8  ! Microphysics cut-off for cloud
 
       integer :: i, j, k, ngrdcol, ncol, lchnk, stncol
-      integer :: begin_height, end_height ! Output from setup_grid call
+      integer :: begin_height(state%ngrdcol), end_height(state%ngrdcol) ! Output from setup_grid call
       real(r8) :: sfc_elevation(state%ngrdcol)  ! Surface elevation
       
       real(r8), dimension(state%ngrdcol,pverp-top_lev+1) :: zt_g, zi_g ! Thermo & Momentum grids for clubb
@@ -970,12 +970,10 @@ contains
       end do
       
       !  Heights need to be set at each timestep.
-      do i=1, ncol
-        call setup_grid_api( pver + 1 - top_lev, sfc_elevation(i), l_implemented,         & ! intent(in)
-                             grid_type, zi_g(i,2), zi_g(i,1), zi_g(i,pver + 1 - top_lev+1), & ! intent(in)
-                             zi_g(i,:), zt_g(i,:),                            & ! intent(in)
-                             gr(i), begin_height, end_height )                  ! intent(out)
-      end do
+      call setup_grid_api( pverp+1-top_lev, ncol, sfc_elevation(1:ncol), l_implemented,  & ! intent(in)
+                           grid_type, zi_g(1:ncol,2), zi_g(1:ncol,1), zi_g(1:ncol,pverp+1-top_lev),   & ! intent(in)
+                           zi_g(1:ncol,:), zt_g(1:ncol,:),                              & ! intent(in)
+                           gr(1:ncol), begin_height(1:ncol), end_height(1:ncol) )        
          
       ! Calculate the distance between grid levels on the host model grid,
       ! using host model grid indices.
